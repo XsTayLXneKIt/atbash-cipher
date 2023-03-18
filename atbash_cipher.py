51a2793bee92e437d28892999da9888d41085531
XsTayLXneKit <azabitiy@gmail.com>
2023-03-17 20:56:31 -0400

    remake_code

--- atbash_cipher.py
+++ atbash_cipher.py
@@ -1,24 +1,19 @@
-import string
+def encode(plain_text):
+    plain_text = plain_text.lower().replace(" ", "")
+    cipher_text = ""
+    for char in plain_text:
+        if char.isalpha():
+            cipher_text += chr(ord("a") + (25 - (ord(char) - ord("a"))))
+        else:
+            cipher_text += char
+    return " ".join([cipher_text[i:i+5] for i in range(0, len(cipher_text), 5)])
 
-def encode(plaintext):
-
-    # create a translation table that maps each letter to its reverse
-    atbash_table = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[::-1])
-    
-    # apply the translation table to the plaintext and remove non-alphabetic characters
-    ciphertext = plaintext.lower().translate(atbash_table, string.punctuation + string.whitespace + string.digits)
-    
-    # group the ciphertext into blocks of 5 letters
-    return ' '.join([ciphertext[i:i+5] for i in range(0, len(ciphertext), 5)])
-    
-
-def decode(ciphertext):
-
-    # create a translation table that maps each letter to its reverse
-    atbash_table = str.maketrans(string.ascii_lowercase[::-1], string.ascii_lowercase)
-    
-    # apply the translation table to the ciphertext and remove non-alphabetic characters
-    plaintext = ciphertext.lower().translate(atbash_table, string.punctuation + string.whitespace + string.digits)
-    
-    # remove the spaces between blocks of letters and return the plaintext
-    return plaintext.replace(' ', '')
+def decode(cipher_text):
+    cipher_text = cipher_text.lower().replace(" ", "")
+    plain_text = ""
+    for char in cipher_text:
+        if char.isalpha():
+            plain_text += chr(ord("a") + (25 - (ord(char) - ord("a"))))
+        else:
+            plain_text += char
+    return plain_text
